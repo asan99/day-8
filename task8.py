64f9e9fa4d7c7a1fb006bbd72bf76091c2209437
turduhodzhaev99 <turduhodzhaev99@gmail.com>
2020-07-17 11:44:58 +0600

    first commit

---
+++ task8.py
@@ -0,0 +1,8 @@
+#Task 3
+
+#Write a program that greets the user by printing the word "Hello", a comma, the name of the user and an exclamation mark after it. See the examples below. Warning. Your program's output should strictly match the desired one, character by character. There shouldn't be any space between the name and
+#the exclamation mark. You can use + operator to concatenate two strings.
+a =input("Hello,what is your name?")
+word = 'Hello, '
+b ="!"
+print(word + a+b)
